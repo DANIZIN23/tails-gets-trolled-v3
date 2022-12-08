a87eac71da318f074ea00f3225148201fa2ecafb
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-12-08 14:35:59 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -80,9 +80,9 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		
 		addChild(new ui.FPSMem(10, 3, 0xFFFFFF));
-		#end
+		
 	}
 
 	public static function setFPSCap(cap:Int)
